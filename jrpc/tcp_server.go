803bee82e51770d43e5ae68fd2fa4ae16b9d99e2
henly <henly.liu@blockshine.com>
2018-03-14 10:50:27 +0800

    replace log.Fatal to Printf

--- jrpc/tcp_server.go
+++ jrpc/tcp_server.go
@@ -13,13 +13,13 @@ func StartTcpServer(port string) error{
 
 	addr, err := net.ResolveTCPAddr("tcp", port)
 	if err != nil {
-		log.Fatal("Tcp jrpc server failed to ResolveTCPAddr ", err.Error())
+		log.Printf("Tcp jrpc server failed to ResolveTCPAddr ", err.Error())
 		return err;
 	}
 
 	listener, err := net.ListenTCP("tcp", addr)
 	if err != nil {
-		log.Fatal("Tcp jrpc server failed to start ", err.Error())
+		log.Printf("Tcp jrpc server failed to start ", err.Error())
 		return err;
 	}
 
@@ -27,7 +27,7 @@ func StartTcpServer(port string) error{
 	for{
 		conn, err := listener.Accept();
 		if err != nil {
-			log.Fatal("Fatal error: ", err.Error())
+			log.Printf("Fatal error: ", err.Error())
 			continue
 		}
 
@@ -40,39 +40,17 @@ func StartTcpServer(port string) error{
 func StartTcpClient(addr string, method string, params string, res *string) error {
 	client, err := rpc.Dial("tcp", addr)
 	if err != nil {
-		log.Fatal("Fatal error: ", err.Error())
+		log.Printf("Fatal error: ", err.Error())
 		return err
 	}
+	defer client.Close()
 
-	//params := "{\"name\", \"bitcoin\"}";
 	err = client.Call(method, params, res)
-
 	if err != nil {
-		log.Fatal("call failed: ", err.Error())
-		return err;
+		log.Printf("call failed: ", err.Error())
+		return err
 	}
-
-	client.Close();
 
 	fmt.Printf("Reply: %s\n", *res)
-	return nil;
-}
-
-func StartTcpClient2(addr string, method string, args interface{}, reply interface{}) error {
-	client, err := rpc.Dial("tcp", addr)
-	if err != nil {
-		log.Fatal("Fatal error: ", err.Error())
-		return err
-	}
-
-	err = client.Call(method, args, &reply)
-	if err != nil {
-		log.Fatal("call failed: ", err.Error())
-		return err;
-	}
-
-	client.Close();
-
-	fmt.Printf("Reply: %s\n", reply)
-	return nil;
+	return nil
 }
